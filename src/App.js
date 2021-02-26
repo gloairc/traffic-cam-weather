8db52ca4e79f94c92b3b34a12cf94bd97b31eccc
Gloria C <gloria_cheng7@yahoo.com>
2021-02-26 23:46:11 +0800

    added in react-bootstrap datepicker, timepicer and search btn, but without submit logic

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 import Header from "./components/Header"
 import DateTime from "./components/DataTime";
 import LocationList from "./components/LocationList";
-import WeatherCamImage from "./components/WeatherCamImage"
+import WeatherCamImage from "./components/WeatherCamImage";
+import axios from "axios";
 
 function App() {
+  //either set up the url for the axios
+  // figure out how to combine the tables lat-long => readable name
+
+  // <DATETIME/>: date input, time input => urlencode => pass up to APP
+  //APP => make 2 axios traffic-cam API & weather API, 
+  // setState for traffic, setState for weather
+  //APP => pass down to readable location list & camera image & weather info to <LocationList/> 
+  // on click of LocationList => => setState in APP => pass down to <WeatherCamImage /> show image & weather
+
+  //USESTATE
+  //useState for axios url
+  //useState for traffic cam API
+  // useState for weather API
+  // useState for chosenlocation
+
   return (
     <div class="container-fluid px-0" id="overall-app-cont">
 
--- src/components/DataTime.js
+++ src/components/DataTime.js
@@ -1,18 +1,46 @@
 import React from "react";
+import {
+    Form,
+    FormControl,
+    Row,
+    Col,
+    Button
+} from "react-bootstrap";
 
 const DateTime = () => {
 
     return (
-        <div class="container-fluid d-flex flex-row" id="date-time-cont">
-            <div>
-                Date Input
-        </div>
-            <div>
-                Time Input
-     </div>
-
+        <div class="container-fluid d-flex" id="date-time-cont">
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Group controlId="date.ControlInput">
+                            <Form.Label>Date:</Form.Label>
+                            <Form.Control
+                                type="date"
+                                title="date"
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="time.ControlInput">
+                            <Form.Label>Time:</Form.Label>
+                            <Form.Control
+                                type="time"
+                                title="time"
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Button variant="dark">
+                            Search
+</Button>
+                    </Col>
+                </Row>
+            </Form>
         </div>
     )
 }
 
 export default DateTime
+
