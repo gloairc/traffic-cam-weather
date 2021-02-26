44ef20b77c3ceceeac1da796534de33467384915
Gloria C <gloria_cheng7@yahoo.com>
2021-02-26 21:04:12 +0800

    first commit: create-react-app

---
+++ src/sample_response/query.js
@@ -0,0 +1,11 @@
+// TRAFFIC CAMERA
+Query: 2021 - 02 - 26T12: 27: 50 + 08: 00
+URL: https://api.data.gov.sg/v1/transport/traffic-images?date_time=2021-02-26T12%3A27%3A50%2B08%3A00
+
+// limit to minutes
+// or let them search second?
+
+
+// WEATHER
+Query: 2021 - 02 - 26T12: 27: 50 + 08: 00
+URL: https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=2021-02-26T12%3A27%3A50%2B08%3A00
