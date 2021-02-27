ea2b8b3b299d5453a8a71b80ed371d38e2b3378b
Gloria C <gloria_cheng7@yahoo.com>
2021-02-27 10:35:14 +0800

    construct encodeURIComponent for API querystring

--- src/components/DataTime.js
+++ src/components/DataTime.js
@@ -1,38 +1,56 @@
-import React from "react";
-import {
-    Form,
-    FormControl,
-    Row,
-    Col,
-    Button
-} from "react-bootstrap";
+import { React, useEffect, useState } from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
+
+// TRAFFIC CAMERA API
+//Query: 2021-02-26T12:27:50+08:00
+//URL: https://api.data.gov.sg/v1/transport/traffic-images?date_time=2021-02-26T12%3A27%3A50%2B08%3A00
 
 const DateTime = () => {
+    const [formData, setFormData] = useState({})
+    // console.log("formData", formData)
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //manipulate the formData {date:YYYY-MM-DD, time:HH:MM} to to form a valid query string
+        //encode the time
+        const date_time = formData.date + "T" + formData.time + ":00+08:00"
+        const encodedDateTime = encodeURIComponent(date_time)
+        // console.log(encodedDateTime)
+    };
 
     return (
         <div class="container-fluid d-flex" id="date-time-cont">
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Row>
                     <Col>
-                        <Form.Group controlId="date.ControlInput">
+                        <Form.Group controlId="dateInput">
                             <Form.Label>Date:</Form.Label>
                             <Form.Control
                                 type="date"
                                 title="date"
+                                onChange={(event) => {
+                                    setFormData((state) => {
+                                        return { ...state, date: event.target.value }
+                                    })
+                                }}
                             />
                         </Form.Group>
                     </Col>
                     <Col>
-                        <Form.Group controlId="time.ControlInput">
+                        <Form.Group controlId="timeInput">
                             <Form.Label>Time:</Form.Label>
                             <Form.Control
                                 type="time"
                                 title="time"
+                                onChange={(event) => {
+                                    setFormData((state) => {
+                                        return { ...state, time: event.target.value }
+                                    })
+                                }}
                             />
                         </Form.Group>
                     </Col>
                     <Col>
-                        <Button variant="dark">
+                        <Button variant="primary" type="submit">
                             Search
 </Button>
                     </Col>
