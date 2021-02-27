a2bc53f2f21e7abcdbb72b01d6d02acb8ca0e51b
Gloria C <gloria_cheng7@yahoo.com>
2021-02-27 09:16:21 +0800

    commented our LocationList axios for the time being

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 const LocationList = () => {
-    const [camera, setCamera] = useState("hi")
+    // const [camera, setCamera] = useState("hi")
 
-    // useEffect = (() => {
-    axios
-        .get("https://api.data.gov.sg/v1/transport/traffic-images?date_time=2021-02-26T12%3A27%3A50%2B08%3A00")
-        .then((response) => {
-            console.log("axios response", response);
-            // setCamera(response.data)
-        })
-        .catch((error) => {
-            console.log("axios error", error);
-        });
-    // }, [])
+    // // useEffect = (() => {
+    // axios
+    //     .get("https://api.data.gov.sg/v1/transport/traffic-images?date_time=2021-02-26T12%3A27%3A50%2B08%3A00")
+    //     .then((response) => {
+    //         console.log("axios response", response);
+    //         // setCamera(response.data)
+    //     })
+    //     .catch((error) => {
+    //         console.log("axios error", error);
+    //     });
+    // // }, [])
 
     return (
         <div class="container-fluid" id="location-list-cont">
             Location List
-            {camera}
+            {/* {camera} */}
         </div>
     )
 }
