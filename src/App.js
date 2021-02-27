606c0ce64a17eb694c890ff33aacdbd14e7b3884
Gloria C <gloria_cheng7@yahoo.com>
2021-02-27 22:01:56 +0800

    merged API 1 & API 2 to form camNforcast array

--- src/App.js
+++ src/App.js
@@ -24,8 +24,9 @@ function App() {
   const [queryString, setQueryString] = useState("")
   const [cameraData, setCameraData] = useState({})
   const [weatherData, setWeatherData] = useState([])
+  const [camNForecastData, setCamNForecastData] = useState([])
 
-  useEffect(() => {
+  useEffect(() => {//setCameraData & setWeatherData
     if (queryString !== "") {//axios API if querystring not empty
       axios.all(
         [axios.get("https://api.data.gov.sg/v1/transport/traffic-images?date_time=" + queryString),
@@ -43,11 +44,9 @@ function App() {
             let correspondingForecast = forecast.filter((forecastData) => forecastData.area === area_metadata[i].name); //[{area:"AMK", forcast:"windy"}]
             //have a combined {} of name, label_location, forecast
             let areaNforecast = { ...area_metadata[i], forecast: correspondingForecast[0].forecast };
-
-            areaNforecastArray.push(areaNforecast)
+            areaNforecastArray.push(areaNforecast);
           }
-          // console.log("areaNforecastArray inside", areaNforecastArray)
-          setWeatherData(areaNforecastArray)
+          setWeatherData(areaNforecastArray);
         }))
         .catch(axios.spread((error1, error2) => {
           console.log("axios traffic error", error1);
@@ -57,6 +56,49 @@ function App() {
   }, [queryString])
 
   console.log("weathdata outside", weatherData)
+  console.log("cameradata outside", cameraData)
+
+
+  // if cameraData & weatherData is not empty, find nearest area for camera based on long & lat
+  let cameraNforecastArray = [];
+  useEffect(() => {
+    if (Object.keys(cameraData).length !== 0 && weatherData.length !== 0) {
+
+      for (let i = 0; i < cameraData.cameras.length; i++) { //walk through camera array 
+        const closestLocation = (targetLocation, locationArray) => {
+          //Step 1: Function to calculate the hypotenus
+          const findHypotenus = (dx, dy) => {
+            return Math.sqrt(dx * dx + dy * dy);
+          }
+          // Step 2: Using input to calculate hypotenus
+          const distanceBtwLocation = (targetLocation, locationArray) => {
+            const dx = targetLocation.latitude - locationArray.label_location.latitude;
+            const dy = targetLocation.longitude - locationArray.label_location.longitude;
+            // console.log("dy", dy, ". dx", dx)
+            return findHypotenus(dx, dy);
+          }
+
+          // Step 3: for each {} in locationArray, calculate the distanceBtwLocation,
+          //return the location nearest to it, if equal distance will return in order of array, hence the one with lowest alphabetical :/
+          return locationArray.reduce((prev, curr) => {
+            let prevDistance = distanceBtwLocation(targetLocation, prev);
+            let currDistance = distanceBtwLocation(targetLocation, curr);
+            return (prevDistance < currDistance) ? prev : curr;
+          });
+        }
+
+        let closest = closestLocation(cameraData.cameras[i].location, weatherData)
+        // console.log("for", cameraData.cameras[i], " cloest location is ", closest)
+
+        //have a combined [{}] of camera, locaitonName, location_longlat, location_forecast
+        let combinedCameraNForecast = { ...cameraData.cameras[i], locationName: closest.name, locationName_longlat: closest.label_location, locationName_forecast: closest.forecast };
+        cameraNforecastArray.push(combinedCameraNForecast)
+      }
+      setCamNForecastData(cameraNforecastArray)
+      console.log("cameraNforecastArray", cameraNforecastArray)
+    }
+  }, [cameraData, weatherData])
+
   return (
     <div class="container-fluid px-0" id="overall-app-cont">
 
@@ -64,7 +106,7 @@ function App() {
 
       <DateTime setQueryString={setQueryString} />
 
-      <LocationList cameraData={cameraData} />
+      <LocationList cameraData={cameraData} camNForecastData={camNForecastData} />
 
       <WeatherCamImage />
 
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -3,11 +3,18 @@ import React, { useEffect, useState } from "react";
 
 const LocationList = (props) => {
     //clean up props.cameraData.timestamp here, remove +08:00, seperate date,time, reorder data & time
-    
+const [cameraId, setCameraId]= useState("")
+    useEffect(()=>{
+        if (props.camNForecastData.length !==0){
+setCameraId(props.camNForecastData[0].camera_id)
+        }
+    },[props.camNForecastData])
+
     return (
         <div class="container-fluid" id="location-list-cont">
             Showing list acquired from LTA's datamall for {props.cameraData.timestamp}
-            
+            {/* table list of location, camera number */}
+            {cameraId}
             
         </div>
     )
